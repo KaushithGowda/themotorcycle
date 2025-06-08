96cb003be4601bad0855a41e167cd2d1d85196f4
Kaushith_Gowda_M <koushith7@gmail.com>
2025-06-08 12:57:10 +0530

    Login form suspense

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,14 @@
-import LoginForm from "@/components/auth/login-form"
+import LoginForm from '@/components/auth/login-form'
+import { Suspense } from 'react'
 
 const LoginPage = async () => {
-    return (
+  return (
+    <div className='h-full flex items-center justify-center'>
+      <Suspense fallback={<div>Loading login form...</div>}>
         <LoginForm />
-    )
+      </Suspense>
+    </div>
+  )
 }
 
 export default LoginPage
